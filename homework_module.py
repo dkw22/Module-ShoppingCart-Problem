ef2547218238ee906c199e718679000af6202a34
dkw22 <64745181+dkw22@users.noreply.github.com>
2021-06-03 17:19:24 -0400

    Inital Commit

---
+++ homework_module.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def square_foot(length, width):
+    area = length * width
+    print(f"The square footage is {area}.")
+
+def circumference(diameter):
+    circle_circumference = pi * diameter
+    print(f"The circumference of the circle is {circle_circumference}.")
